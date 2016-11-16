cd7e7c11915f916524762eddfadbece04a20b477
Nick Lyman <nick.lyman@gmail.com>
2016-11-16 12:02:48 -0800

    Create new object, loop through each letter to check if it is in object yet or not, print out object key value pairs to screen.

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,21 @@
 //business logic
 
 var userString = function(submittedText) { //Get user submitted text
-  for (var i = 0; i < submittedText.length; i++) { //Loop through the text
-    if (i >= 0) {
-      $(".output").append("<ul>" + (submittedText[i]) + "</ul>") //List out letters on index.html
-      console.log(submittedText[i]);
+  var letterCount = {}; //Create new empty object
+    for (var i = 0; i < submittedText.length; i++) { //Loop through each character in user submitted text
+    var letter = submittedText.charAt(i); //Looks at each character from submittedText
+    if (letterCount[letter]) { //Checks if letter is already part of object
+      letterCount[letter]++; //If yes, 1 is added to tally for that letter
+    } else {
+      letterCount[letter] = 1; //If no, 1 is entered for the tally of that letter
     };
   };
+  Object.keys(letterCount).forEach(function(key) { //Loops through each key value pair 
+  $(".output").append("<ul>" + (key) + " : " + (letterCount[key]) + "</ul>"); //List out letters and number of occurances on index.html
+  });
+  return letterCount; //Object is returned
 };
+
 
 
 
