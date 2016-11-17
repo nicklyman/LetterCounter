52eaf4476336efe0b8c35a7aa3468514897528be
Nick Lyman <nick.lyman@gmail.com>
2016-11-17 11:19:15 -0800

    Update comments to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@
 var userString = function(submittedText) { //Get user submitted text
   var letterCount = {}; //Create new empty object
   $(".output").append("<h3>" + (submittedText) + "</h3>"); //Displays user submission text above list of letters and letter count
-  var onlyLetters = submittedText.toUpperCase().split(" ").join("").replace(/[^A-Z]/g, ""); //Keeps only letters in string for analysis
+  var onlyLetters = submittedText.toUpperCase().split(" ").join("").replace(/[^A-Z]/g, ""); //Keeps only letters in string for analysis, removes whitespace, converts all letters to uppercase
     for (var i = 0; i < onlyLetters.length; i++) { //Loop through each character in user submitted text
     var letter = onlyLetters.charAt(i); //Looks at each character from submittedText
     if (letterCount[letter]) { //Checks if letter is already part of object
@@ -25,11 +25,9 @@ $(document).ready(function() {
   $("form#userInput").submit(function(event) { //User text is submitted from index.html form
     $("ul,h3").empty(); //Clears p tag and ul list after new text is submitted
     event.preventDefault();
-
     var submittedText = $("input#value").val(); //User text from form converted to upper case and all white space removed
     $("input#value").val(""); //Clears field after submit button
     var output = userString(submittedText); //Result of analysis conducted in business logic
-
     $(".output").show(); //Displays output
   });
 });
